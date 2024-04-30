17f98365623e3e50aa5f2e7dfc52dd0061dd2aa1
Tufail Ahmed <muhammad.tufail@savyour.com>
2024-04-30 11:19:23 +0500

    Done with LLM

--- django/api/chatbot.py
+++ django/api/chatbot.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.http import require_GET, require_POST
 from django.views.decorators.csrf import csrf_exempt
+from api.llm.huggingface import huggingface
 
 @require_POST
 @csrf_exempt
 def getMessage(request):
     userText = request.POST.get('text')
-    return HttpResponse(userText)
+
+    llmClassObject = huggingface('mistralai/Mistral-7B-Instruct-v0.2')
+    llmResult = huggingface.getResult(llmClassObject, {"inputs": userText})
+    llmResult = llmResult[0]['generated_text']
+    return HttpResponse(llmResult)
---
+++ django/api/llm/huggingface.py
@@ -0,0 +1,14 @@
+import requests
+
+class huggingface:
+    def __init__(self,model):
+        self.apiUrl = "https://api-inference.huggingface.co/models/" + model
+        self.headers = {"Authorization": "Bearer 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"}
+
+    
+    def getResult(self,payload):
+        try:
+            response = requests.post(self.apiUrl, headers=self.headers, json=payload)
+            return response.json()
+        except Exception as e:
+            return e
